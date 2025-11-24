df53ff4795d407bf1bbcdd958909827ebb3c9cf2
mr.sherry <mr.sherry6967@gmail.com>
2025-11-24 17:13:35 +0500

    js start with variables an d

---
+++ b2/js/lec1/index.js
@@ -0,0 +1,37 @@
+// let var const
+
+var hello = 6576;
+let hello1 = 92991;
+const hello3 = 329 - +238;
+
+// hello3 = 8327492838797888888888888878767n;
+
+let bool = false;
+console.log(bool);
+
+let data = ['ahmed', 'ali', 'raza', 'shafqat'];
+let data1 = [123, 5, 67, 3, 2, 56];
+let students = { rollNo: 543, name: 'ahmed', marks: 655 };
+console.log(typeof students);
+console.log(typeof data1);
+
+let username = "ahmed@123';';...;[?";
+console.log(typeof username);
+
+let number = 888;
+
+let class3;
+console.log(typeof class3);
+
+let data7 = null;
+console.log(typeof data7);
+
+// number call
+
+number = 987;
+
+// function {
+//   343
+// }
+
+// number call
