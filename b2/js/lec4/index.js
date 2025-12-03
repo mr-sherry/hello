3b4e8de479d5960a3a12ceb1fd7d754e0c6ae93b
mr.sherry <mr.sherry6967@gmail.com>
2025-12-03 17:00:37 +0500

    array and methods of array

---
+++ b2/js/lec4/index.js
@@ -0,0 +1,33 @@
+let data = ['helllo', 'hi', 'hy'];
+let data1 = ['helllo', 'hi', 'hy'];
+
+console.log(data.toString());
+console.log(data.join(''));
+console.log(data.pop());
+console.log(data);
+console.log(data.push('ahmed'));
+
+console.log(data.shift());
+console.log(data.unshift('new word'));
+console.log(data.toString());
+// console.log(Array.isArray(data));
+
+console.log(data.concat(data1));
+console.log(data.copyWithin(0, 0, 1));
+console.log(data);
+
+let array = [
+  13242,
+  ['sasd', 'sfasd', 'ooooo'],
+
+  3213,
+  456546,
+  [9999, [98234, 9284, 3345], 8888, 6666],
+  234,
+  6345,
+  53453,
+];
+console.log(array[4].flat(1));
+
+// level2;
+// index;
