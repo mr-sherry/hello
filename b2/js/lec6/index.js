d05100755b06e0111ebd6e6d19f57234dfb0e05e
mr.sherry <mr.sherry6967@gmail.com>
2025-12-05 17:06:07 +0500

    switch and ternary

---
+++ b2/js/lec6/index.js
@@ -0,0 +1,59 @@
+let num = 0;
+
+let roles = ['admin', 'student', 'teacher'];
+
+let loggedin = '';
+
+switch (loggedin) {
+  case 'admin':
+    console.log('admin is looged in');
+
+    break;
+  case 'student':
+    console.log('student is looged in');
+
+    break;
+  case 'teacher':
+    console.log('teacher is looged in');
+
+    break;
+
+  default:
+    console.log('please login');
+
+    break;
+}
+
+// switch (num) {
+//   case 0:
+//     console.log('zero number');
+//     break;
+//   case 10:
+//     console.log('ten number');
+//     break;
+//   case 20:
+//     console.log('twenty number');
+//     break;
+//   case 30:
+//     console.log('thirty number');
+//     break;
+
+//   default:
+//     console.log('no case worked');
+
+//     break;
+// }
+
+// if (condition) {
+// } else {
+// }
+
+let user = false;
+
+user === true ? console.log('user logged') : console.log('user not logged');
+
+// condition && code;
+
+if (condition || condition) {
+    
+}
